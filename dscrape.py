639f14cceb93166178224b84a08c77f202c317e0
Jason Hite <jasonmhite@gmail.com>
2012-02-11 22:19:24 -0500

    Still coming together
    This is awful code, needs a lot of sanity. Also, I don't know anything
    about OOP >.<.

--- dscrape.py
+++ dscrape.py
@@ -9,34 +9,92 @@ import Queue
 import threading
 
 class MyDaemon(Daemon):
+
     def loadqueue(self, configfile):
         self.defaults = {"zeropaddedq" : "true"}
         self.configfile = configfile
         self.q = Queue.Queue()
-    def readconfig(self):
+        self.lock = threading.Lock()
+
+    def update(self):
         self.currentable = []
-        try:
-            self.config = ConfigParser.SafeConfigParser(defaults)
-            self.config.read(configfile)
-        except:
-            raise NameError('Config Parse Error')
-        for sect in config.sections():
-            searchstring = self.config.get(sect, 'sstring')
-            episode = self.config.get(sect, 'currentepisode')
-            url = self.config.get(sect, 'feedurl')
-            feed = feedparser.parse(url)
-            zeropaddedepnum = self.config.get(sect, "zeropaddedq")
-            if zeropaddedepnum == 'true' and int(episode) < 10:
-                episode = "0" + episode
-            query = searchstring.replace("*EP", episode)
-            self.currentable.append((sect,query,feed))
-    def scan(self):
-        
+        with self.lock:
+            try:
+                self.config = ConfigParser.SafeConfigParser(defaults)
+                self.config.read(configfile)
+            except:
+                raise NameError('Config Parse Error')
+            for sect in config.sections():
+                searchstring = self.config.get(sect, 'sstring')
+                episode = self.config.get(sect, 'currentepisode')
+                url = self.config.get(sect, 'feedurl')
+                feed = feedparser.parse(url)
+                zeropaddedepnum = self.config.get(sect, "zeropaddedq")
+                if zeropaddedepnum == 'true' and int(episode) < 10:
+                    episode = "0" + episode
+                query = searchstring.replace("*EP", episode)
+                self.currentable.append((sect,query,feed))
+            for entry in self.currenttable:
+                hit = self.scan_query(entry)
+            if hit:
+                self.q.put(hit)
+               
+    def scan_query(self,item):
+        res = False
+        for entry in item[2].entries:
+            title = filter(lambda x: x in string.printable, entry.title)
+            if item[1] in title:
+                res = (nib[0], nib[1], entry.link)
+            return(res)
+        return(res)
+
     def run(self):
         self.loadqueue()
         with open('testoutput.txt', 'w') as thefile:
             thefile.write('hello')
             sleep(10)
+
+    class Worker(threading.Thread): 
+        def __init__(self, queue, configfile):
+            self.__queue = queue
+            self.plock = lock
+            self.configfile = configfile
+            self.matches = []
+            threading.Thread.__init__(self)
+
+        def download(self, item):
+            try:
+                dl = urllib2.urlopen(item[2], timeout=2)
+                with open("/Users/jmhite/Automatically Downloaded/" + item[1] + '.torrent', 'w') as outfile:
+                    outfile.write(dl.read())
+                self.matches.append(item)
+            except:
+                raise NameError('Download Error')
+        
+        def update(self):
+            with self.plock:
+                try:
+                    newconfig = ConfigParser.SafeConfigParser()
+                    newconfig.read(configfile)
+       
+                for m in matchlist:
+                    i = int(newconfig.get(m[0], "currentepisode")) + 1
+                    newconfig.set(m[0], "currentepisode", str(i))
+                with open(configfile, "wb") as updated:
+                    newconfig.write(updated)
+                except:
+                    raise NameError('Config Replace Error')
+
+        def run(self):
+            while True:
+                if queue.empty():
+                    self.update(self.matches)
+                    break
+                item = self.__queue.get()
+                try:
+                    queue.task_done()
+
+
 
 if __name__ == "__main__":
     daemon = MyDaemon('/tmp/daemon-example.pid')
